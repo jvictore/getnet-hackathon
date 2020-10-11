86728f37fbb05a20e5516acf6898813e06b798d5
vittoi <vitor.carvalho@dcomp.sor.ufscar.br>
2020-10-11 03:03:06 -0300

    resto das telas e o botao de venda se pa ja ta on

--- app/src/main/java/com/example/hackathon/index_activity.java
+++ app/src/main/java/com/example/hackathon/index_activity.java
@@ -2,7 +2,10 @@ package com.example.hackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class index_activity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class index_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.index_activity);
+
+        Button btn_venda = findViewById(R.id.btnVenda);
+        btn_venda.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(index_activity.this, telavenda_activity.class);
+                startActivity(it);
+            }
+        });
     }
 }
