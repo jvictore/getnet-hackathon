0f246247b4c7a0518a72a0d108fcecbb76d19806
Alex Junior <alexmomijunior@gmail.com>
2020-10-11 16:26:30 -0300

    banco de dados

--- app/src/main/java/com/example/hackathon/index_activity.java
+++ app/src/main/java/com/example/hackathon/index_activity.java
@@ -1,13 +1,25 @@
 package com.example.hackathon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class index_activity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +29,32 @@ public class index_activity extends AppCompatActivity {
         Button btn_venda = findViewById(R.id.btnVenda);
         btn_venda.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent it = new Intent(index_activity.this, telavenda_activity.class);
+                Intent it = new Intent(index_activity.this, telacompra_activity.class);
                 startActivity(it);
             }
         });
+
+/*
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> umFluxo = new HashMap<>();
+        umFluxo.put("User", "Acknowlogia");
+        umFluxo.put("senha", "alex07");
+        umFluxo.put("email", "alexmomi@hotmail.com");
+        db.collection("Usuarios")
+                .add(umFluxo)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("TAG", "Error adding document", e);
+                    }
+                });
+
+ */
     }
 }
--- app/src/main/java/com/example/hackathon/telacompra_activity.java
+++ app/src/main/java/com/example/hackathon/telacompra_activity.java
@@ -1,14 +1,54 @@
 package com.example.hackathon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.String.*;
 
 public class telacompra_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.telacompra_activity);
     }
+
+    public void salvarConta(View view){
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    EditText valor = (EditText) findViewById(R.id.editTextNumber);
+    Float valor_float = Float.parseFloat(valueOf(valor.getText()));
+
+    Map<String, Object> umaReceita = new HashMap<>();
+    umaReceita.put("valor", valor_float);
+
+    db.collection("Fluxo/dividas/padealho")
+            .add(umaReceita)
+            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w("TAG", "Error adding document", e);
+                }
+            });
+}
 }
