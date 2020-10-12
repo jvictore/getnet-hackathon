f1a8e2c3ef9569d1201fcc64c6cabcbcc043cf69
Alex Junior <alexmomijunior@gmail.com>
2020-10-12 00:22:03 -0300

    criacao de categoria

--- app/src/main/java/com/example/hackathon/addcategoria_activity.java
+++ app/src/main/java/com/example/hackathon/addcategoria_activity.java
@@ -1,8 +1,24 @@
 package com.example.hackathon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
 
 public class addcategoria_activity extends AppCompatActivity {
 
@@ -10,5 +26,47 @@ public class addcategoria_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addcategoria_activity);
+    }
+
+    public void nova_categoria(View view) {
+        Boolean tipo = null;
+        String GV = getIntent().getStringExtra("TYPE");
+        Date now = new Date();
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT-3"));
+        Map<String, Object> mapa = null;
+
+        if (GV.equals("VENDA")) {
+            tipo = true;
+        }
+        else {
+            tipo = false;
+        }
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        EditText texto = (EditText) findViewById(R.id.editTextTextPersonName);
+        String nome_categoria = texto.getText().toString();
+        Map<String, Object> categoria = new HashMap<>();
+        categoria.put("categoria", nome_categoria);
+        categoria.put("tipo", tipo);
+        categoria.put("att", dateFormat.format(now));
+        categoria.put("valor_total", df.format(0));
+
+        db.collection("Categorias")
+                .add(categoria)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("addcategoria", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("addcategoria", "Error adding document", e);
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/hackathon/venda_activity.java
+++ app/src/main/java/com/example/hackathon/venda_activity.java
@@ -1,21 +1,31 @@
 package com.example.hackathon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class venda_activity extends AppCompatActivity {
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+import java.util.Map;
+
+public class venda_activity extends AppCompatActivity {
+    public static final String TAG = "venda_activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.venda_activity);
     }
 
     public void addCategoria(View view)
     {
-        startActivity(new Intent(this, addcategoria_activity.class));
+        Intent intent = new Intent(this, addcategoria_activity.class);
+        intent.putExtra("TYPE", "VENDA");
+        startActivity(intent);
     }
 }
